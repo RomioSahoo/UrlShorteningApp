770a477812f0d6ccea4243a5ed710725baea2938
unknown <romior477.com>
2021-01-02 11:23:03 +0530

    RESTFUL web services

--- MvcUsingJsp/src/main/java/com/cognizant/hibernateTest/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.hibernateTest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.hibernateTest.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
-}
--- MvcUsingThymeLeaf/src/main/java/com/cognizant/hibernateTest/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.hibernateTest.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.hibernateTest.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long>{
-
-}
